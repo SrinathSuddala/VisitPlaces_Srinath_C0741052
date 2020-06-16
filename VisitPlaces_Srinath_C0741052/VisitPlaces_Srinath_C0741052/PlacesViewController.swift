a1b0db24d2618cded980888451b05d65edced188
Srinath <srinathsuddala@gmail.com>
2020-06-16 06:35:59 +0530

    Added table view

--- VisitPlaces_Srinath_C0741052/VisitPlaces_Srinath_C0741052/PlacesViewController.swift
+++ VisitPlaces_Srinath_C0741052/VisitPlaces_Srinath_C0741052/PlacesViewController.swift
@@ -1,13 +1,13 @@
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class PlacesViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
